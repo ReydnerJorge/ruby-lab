a04b81bc6d10079caf9b96cd022a827ac1a2f245
ReydnerJorge <reydneroliveirajorge96@gmail.com>
2025-02-24 15:44:58 -0300

    code challenge

---
+++ desafios/nome-completo-idade.rb
@@ -0,0 +1,16 @@
+=begin
+1) Crie um programa que dê a saída do nome completo do usuário e a sua idade
+=end
+
+# nome do usuário
+puts ("Digite o seu nome")
+nome = gets.chomp
+# sobrenome do usuário
+puts ("Digite o seu Sobrenome")
+sobrenome = gets.chomp
+# idade do usuário 
+puts ("Digite a sua idade")
+idade = gets.chomp
+
+puts ("Meu nome é #{nome} #{sobrenome}, é tenho #{idade} anos!")
+
