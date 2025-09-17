c379264485ccb4013dbcb02a34d302ae1aaae2b5
ReydnerJorge <reydneroliveirajorge96@gmail.com>
2025-09-17 19:06:52 -0300

    fix:

--- aula04/operadores.rb
+++ aula04/operadores.rb
@@ -3,8 +3,21 @@ num2 = 9
 num3 = 8
 num4 = 7
 
+
+adicao = (num1 + num2)
+subtracao = (num2 - num3)
+multiplicacao = (num1 * num2)
+divisao = (num1 / num2)
+modulo  = (num1 % num2)
+expoente = (num1 ** num2)
 resultado = ((num1 + num2) - (num3 + num4) / 4) * 6**2 
 puts resultado
+puts adicao
+puts subtracao
+puts multiplicacao
+puts divisao
+puts modulo
+puts expoente
 
 =begin
 adição: +
